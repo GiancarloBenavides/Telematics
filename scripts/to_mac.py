c9ff01201240b58fc3f6acbbab0a87127f56a72b
Giancarlo <giancarlobenavides@hotmail.com>
2023-08-13 05:03:42 -0500

    fix: add docstring

--- scripts/to_mac.py
+++ scripts/to_mac.py
@@ -3,8 +3,9 @@ import subprocess
 import optparse
 import re
 
+
 def get_arguments() -> list:
-    ''' Cambiar la dirección MAC de una interface. '''
+    ''' Obtener las opciones y argumentos ingresados por consola. '''
     parser = optparse.OptionParser(usage="%prog [OPTION]... [NEW MAC]...")
     parser.add_option(
         "-i", "--interface", dest="interface", help="Source Interface")
@@ -15,10 +16,11 @@ def get_arguments() -> list:
         parser.error("Not defined new mac, use --help")
     return options.interface, args[0]
 
-def get_mac(interface):
-    ''' Obtener la dirección MAC de una interface. '''
+
+def get_mac(interface: str) -> str:
+    ''' Obtener en Linux la dirección MAC de una interface. '''
     results = subprocess.check_output(["ifconfig", interface])
-    mac = re.search(r"(\w\w:){5}\w\w", str(results))    
+    mac = re.search(r"(\w\w:){5}\w\w", str(results))
     if mac:
         return mac.group(0)
     else:
@@ -26,15 +28,18 @@ def get_mac(interface):
 
 
 def changue_mac(interface: str, mac: str) -> None:
-    ''' Cambiar la dirección MAC de una interface. '''
+    ''' Cambiar en Linux la dirección MAC de una interface. '''
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-interface, new_mac = get_arguments()
-mac = get_mac(interface)
-with open('bck.txt', 'a') as file:
-    file.write(f"{mac}\n")
 
-# echo mac>> bck-mac.txt
-print(mac.group(0))
+if __name__ == "__main__":
+    interface, new_mac = get_arguments()
+    mac = get_mac(interface)
+
+    with open('bck.txt', 'a') as file:
+        file.write(f"{mac}\n")
+
+    # echo mac>> bck-mac.txt
+    print(mac.group(0))
